989a0bf9433ee97ee7f66be9bb16f0127c505e06
chenjumin <chenjumin@163.com>
2019-01-09 21:57:17 +0800

    hystrix dashboard

--- hystrix-dashboard/src/main/java/com/seasy/springcloud/hystrix/Main.java
+++ hystrix-dashboard/src/main/java/com/seasy/springcloud/hystrix/Main.java
@@ -2,12 +2,8 @@ package com.seasy.springcloud.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
-@EnableFeignClients
 @SpringBootApplication
 @EnableHystrixDashboard
 public class Main {
