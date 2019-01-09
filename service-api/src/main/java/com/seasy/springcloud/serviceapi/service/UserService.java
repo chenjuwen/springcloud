5f6d7a99fa3526c08584aef6977c702c60b9b5a4
chenjumin <chenjumin@163.com>
2019-01-09 17:32:51 +0800

    1

--- service-api/src/main/java/com/seasy/springcloud/serviceapi/service/UserService.java
+++ service-api/src/main/java/com/seasy/springcloud/serviceapi/service/UserService.java
@@ -6,33 +6,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.seasy.springcloud.serviceapi.bean.Address;
 import com.seasy.springcloud.serviceapi.bean.User;
 
-@RequestMapping("/user")
 public interface UserService {
-	@GetMapping("/{id}")
+	@GetMapping("/user/{id}")
 	String addUser(@PathVariable(value="id") Long id);
 	
-	@GetMapping("/query")
+	@GetMapping("/user/query")
 	String query(@RequestParam(value="name") String name);
 	
-	@GetMapping("/getUser")
+	@GetMapping("/user/getUser")
 	User getUser(@RequestParam(value="id") Long id);
 	
-	@PostMapping("/add")
+	@PostMapping("/user/add")
 	User add(@RequestBody Address address);	
 	
-	@PostMapping("/update")
+	@PostMapping("/user/update")
 	String update(@RequestBody Address address);
 	
-	@GetMapping("/getAllAddress")
+	@GetMapping("/user/getAllAddress")
 	List<Address> getAllAddress();
 	
-	@PostMapping("/put")
+	@PostMapping("/user/put")
 	void put(@RequestBody Address address);
 	
 }
