be1cb7f8b92b389f0f0b5a48f310c57163f1d707
chenjumin <chenjumin@163.com>
2019-01-10 20:25:08 +0800

    1

--- hystrix-turbine/src/main/java/com/seasy/springcloud/turbine/Main.java
+++ hystrix-turbine/src/main/java/com/seasy/springcloud/turbine/Main.java
@@ -2,11 +2,10 @@ package com.seasy.springcloud.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
-@EnableHystrixDashboard //启用Hystrix Dashboard功能
+//@EnableHystrixDashboard //启用Hystrix Dashboard功能
 @EnableTurbine //开启Turbine
 public class Main {
 	public static void main(String[] args) {
