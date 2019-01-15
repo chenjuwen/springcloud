23cc1289aaab46d11fff6ac404b7255aae735f85
chenjumin <chenjumin@163.com>
2019-01-15 22:51:49 +0800

    1

---
+++ service-zuul/src/main/java/com/seasy/springcloud/zuul/filter/DefaultZuulFilter.java
@@ -0,0 +1,72 @@
+package com.seasy.springcloud.zuul.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+/**
+ * 自定义Filter
+ */
+@Component
+public class DefaultZuulFilter extends ZuulFilter {
+	/**
+	 * 过滤器类型，它决定过滤器在请求的哪个生命周期中执行。
+	 * 		pre：路由之前
+	 * 		routing：路由之时
+	 * 		post： 路由之后
+	 * 		error：发送错误调用
+	 */
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+	
+	/**
+	 * filter执行顺序
+	 * 数字越大，优先级越低
+	 */
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+	
+	/**
+	 * 判断该过滤器是否需要被执行。
+	 */
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+	
+	/**
+	 * 过滤器的具体逻辑：可以查询数据库判断是否有访问权限
+	 */
+	@Override
+	public Object run() {
+		RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        System.out.println(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+        	System.out.println("token is empty");
+            requestContext.setSendZuulResponse(false); //令 Zuul 过滤该请求，不对其进行路由
+            requestContext.setResponseStatusCode(401); //返回的错误码
+            
+            try {
+                requestContext.getResponse().getWriter().write("token is empty");
+            }catch (Exception ex){
+            	ex.printStackTrace();
+            }
+            
+            return null;
+        }
+        
+        System.out.println("ok");
+        return null;
+	}
+	
+}
