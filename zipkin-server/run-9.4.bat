39df8ddef1c22ea9ceae533d477a941796a86ad4
chenjumin <chenjumin@163.com>
2019-01-24 11:41:30 +0800

    1

--- zipkin-server/run-9.4.bat
+++ zipkin-server/run-9.4.bat
@@ -1,2 +1,2 @@
-java -jar zipkin-server-2.9.4-exec.jar
+java -jar zipkin-server-2.9.4-exec.jar --STORAGE_TYPE=mysql --MYSQL_HOST=localhost --MYSQL_TCP_PORT=3306 --MYSQL_USER=root --MYSQL_PASS=root --MYSQL_MAX_CONNECTIONS=100
 pause
