ace3100ae8ddd2133233da147d1e5c3a45a70b0b
chenjumin <chenjumin@163.com>
2019-02-24 22:00:47 +0800

    spring boot admin server

---
+++ spring-boot-admin-server/src/main/java/com/seasy/adminserver/Main.java
@@ -0,0 +1,14 @@
+package com.seasy.adminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class Main {
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
