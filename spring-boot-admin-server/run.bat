f2351444f5f47339c0ddbd48de62193c3148f00c
chenjumin <chenjumin@163.com>
2019-03-27 22:49:46 +0800

    admin server

---
+++ spring-boot-admin-server/run.bat
@@ -0,0 +1,2 @@
+java -Xms128m -Xmx512m -Dfile.encoding=utf-8 -jar spring-boot-admin-server.jar
+pause
---
+++ spring-boot-admin-server/run.sh
@@ -0,0 +1,1 @@
+java -Xms128m -Xmx512m -Dfile.encoding=utf-8 -jar spring-boot-admin-server.jar &
